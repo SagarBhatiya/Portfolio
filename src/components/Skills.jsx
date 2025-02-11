62a12ee32099aca0ebe573279766798592c1270a
Sagar Bhatiya <sagarbhatiya12211@gmail.com>
2025-02-11 17:17:03 +0530

    11 feb 2025

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,12 @@
+import Cards from "./Cards";
+
 function Skills() {
     return ( 
-        <div id="skill" className="h-screen w-full bg-green-100">
-            skill
+        <div id="skill" className="h-screen w-full bg-green-100 relative">
+           <div className=" flex flex-col absolute top-10 gap-10">
+            <h1 className="text-4xl font-semibold text-white text-center ">Skills</h1>
+            <Cards/>
+           </div>
         </div>
      );
 }
