05630d5145b28d3ce2ed315a6e28e6938a332bf8
Sagar Bhatiya <sagarbhatiya12211@gmail.com>
2025-02-12 23:44:20 +0530

    12-0202025

---
+++ src/components/Cardss.jsx
@@ -0,0 +1,19 @@
+const Cardss = () => {
+    const Cards = [
+        {id:1,title:"React.js",img:""},
+        {id:2,title:"HTML",img:""},
+        {id:3,title:"CSS",img:""},
+        {id:4,title:"JavaScript",img:""},
+        {id:5,title:"Tailwind Css",img:""},
+        
+    ]
+    return ( 
+      <div className="h-[5vh] flex gap-3 w-[30vw] rounded-lg border border-white border-solid flex justify-center items-center z-10">
+        <div className="h-[4vh] w-[8vw] bg-red-100"></div>
+        <p className="text-white">React.js</p>
+      </div>
+    );
+  };
+  
+  export default Cardss;
+  
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,7 +2,7 @@ import Cards from "./Cards";
 
 function Skills() {
     return ( 
-        <div id="skill" className="h-screen w-full bg-green-100 relative">
+        <div id="skill" className="h-screen w-full bg-[#13131F] relative">
            <div className=" flex flex-col absolute top-10 gap-10">
             <h1 className="text-4xl font-semibold text-white text-center ">Skills</h1>
             <Cards/>
